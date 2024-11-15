05163e0f96b8a0b1de5248dd5d36f9740ead51e5
FilipHans <filip.hansen98@gmail.com>
2024-11-15 15:43:29 +0100

    got github API

--- script.js
+++ script.js
@@ -4,13 +4,45 @@ const toProjectBtn = document.querySelector('.go-to-projects');
 const projects = document.querySelector('.projects-page');
 const toAboutBtn = document.querySelector('.go-to-about');
 
+const aboutMeText = document.querySelector('.phone-text');
+
+const experienceBox1 = document.querySelector('.experience-box1');
+const experienceBox2 = document.querySelector('.experience-box2');
+const experienceBox3 = document.querySelector('.experience-box3');
+
+
 fetch("resumeData/resume.json")
     .then(result => result.json())
     .then(value => resumePrinter(value))
 
 function resumePrinter (data) {
+    
+    const {basics, work, education, skills, languages} = data;
+    const {email, phone, name, location, profiles, summary} = basics;
 
-    console.log(data);
+    aboutMeText.textContent = summary;
+    experienceBox1.innerHTML = `<p>${work[0].position}</p>
+                                <span class="green-time">Full Time</span>
+                                <span class="institute"><img src="./img/apartment_20dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg" alt="Apartment"> ${work[0].company}</span>
+                                <span class="location"><img src="./img/location_on_20dp_5F6368_FILL0_wght400_GRAD0_opsz20.svg" alt="Pindrop">${work[0].location}</span>
+                                <span class="calendar"><img src="./img/calendar_month_20dp_5F6368_FILL0_wght400_GRAD0_opsz20.svg" alt="Calendar"> <time datetime="2018-05">${work[0].startDate}</time> - <time datetime="2019-05">${work[0].endDate}</time>
+                                </span>`
+    experienceBox2.innerHTML = `<p>${work[1].position}</p>
+                                <span class="green-time">Full Time</span>
+                                <span class="institute"><img src="./img/apartment_20dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg" alt="Apartment"> ${work[1].company}</span>
+                                <span class="location"><img src="./img/location_on_20dp_5F6368_FILL0_wght400_GRAD0_opsz20.svg" alt="Pindrop">${work[1].location}</span>
+                                <span class="calendar"><img src="./img/calendar_month_20dp_5F6368_FILL0_wght400_GRAD0_opsz20.svg" alt="Calendar"> <time datetime="2018-05">${work[1].startDate}</time> - <time datetime="2019-05">${work[1].endDate}</time>
+                                </span>`
+    experienceBox3.innerHTML = `<p>${work[2].position}</p>
+                                <span class="green-time">Full Time</span>
+                                <span class="institute"><img src="./img/apartment_20dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg" alt="Apartment"> ${work[2].company}</span>
+                                <span class="location"><img src="./img/location_on_20dp_5F6368_FILL0_wght400_GRAD0_opsz20.svg" alt="Pindrop">${work[2].location}</span>
+                                <span class="calendar"><img src="./img/calendar_month_20dp_5F6368_FILL0_wght400_GRAD0_opsz20.svg" alt="Calendar"> <time datetime="2018-05">${work[2].startDate}</time> - <time datetime="2019-05">${work[2].endDate}</time>
+                                </span>`
+
+    console.log(education[0].institution);
+    console.log(data)
+
 }
 
 toProjectBtn.addEventListener('click', () => {
@@ -24,9 +56,9 @@ toAboutBtn.addEventListener('click', () => {
     projects.style.display = 'none';
 })
 
-// const gitHubAPI = `https://raw.githubusercontent.com/FilipHans/quizTest/tree/main/testingCode`;
 
-// const gitHubAPI = "https://github.com/FilipHans/quizTest/tree/main/testingCode";
+const gitHubAPI = `https://api.github.com/users/FilipHans/repos`;
+
 
 async function apiGetter () {
 
@@ -48,4 +80,7 @@ async function apiGetter () {
 
     
 }
+
+const data = await apiGetter();
+
 
